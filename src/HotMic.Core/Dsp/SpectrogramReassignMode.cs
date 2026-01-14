0384b892e62e447e4ba0344e38e93bfd20832776
Chris Arsenault <carsena2@gmail.com>
2026-01-13 17:48:22 -0700

    Add spectrogram reassignment mode enum

---
+++ src/HotMic.Core/Dsp/SpectrogramReassignMode.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HotMic.Core.Dsp;
+
+/// <summary>
+/// Controls time and/or frequency reassignment for spectrogram displays.
+/// </summary>
+[Flags]
+public enum SpectrogramReassignMode
+{
+    Off = 0,
+    Frequency = 1,
+    Time = 2,
+    TimeFrequency = Frequency | Time
+}
